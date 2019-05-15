1c57c048a77f614efc217c4aea8538c127801c31
Daniel Buchmann <daniel.buchmann@unit.no>
2019-05-15 12:38:55 +0200

    Nå funker det

--- Jenkinsfile-deploy.groovy
+++ Jenkinsfile-deploy.groovy
@@ -5,24 +5,16 @@ pipeline {
         maven 'Maven 3.6.1'
     }
 
-
-//    parameters {
-//        choice(name: 'Destination', choices: ['brage-utvikle.bibsys.no', 'brage-test.bibsys.no']
-//                , description: 'Where do you want to deploy to?')
-//    }
-
     environment {
         VERSION = "${env.BRANCH_NAME}".replaceAll('/', '_').toLowerCase()
 		DEVSTEP = "utvikle"
-        // TARGET_HOST = "${params.Destination}"
-        TARGET_HOST = "brage-utvikle.bibsys.no"
         CUSTOMZ = "customizations"
     }
 
 
     stages {
 
-        stage('Select development phase') {
+        stage('Provide input parameters') {
             steps {
 				script {
 					def institusjoner = readYaml file: "ansible/institusjoner.yml"
@@ -30,7 +22,6 @@ pipeline {
 
 					institusjoner.each { prop, val ->
 						if (prop in ["metaClass","class"]) return
-						echo "${prop} = ${val}"
 						kunder << prop
 					}
 
@@ -70,21 +61,19 @@ pipeline {
                 script {
                     try {
                         timeout(activity: true, time: 120, unit: 'SECONDS') {
-                            input(message: "Deploy branch: $VERSION to phase: $DEVSTEP?")
+                            input(message: "Deploy branch $VERSION for $KUNDE to phase $DEVSTEP?")
                         }
                     } catch (err) {
                         println("Release aborted")
                         throw err
                     }
                 }
-                println("Deploying branch $VERSION to server: $TARGET_HOST")
+                println("Deploying branch $VERSION for $KUNDE to $DEVSTEP")
             }
         }
 
 		stage('Checkout Brage6 customizations') {
             steps {
-
-//                println("Running build #${env.BUILD_ID} of job ${env.JOB_NAME}, git branch: ${env.BRANCH_NAME}" as java.lang.Object)
                 script {
                     brageVars = checkout scm
                     dir("${CUSTOMZ}") {
@@ -95,52 +84,6 @@ pipeline {
             }
         }
 
-/*
-        stage('Select Institution') {
-            steps {
-                script {
-                    env.WORKSPACE = pwd();
-                    def file = readFile "${ENV_FOLDER}/env/inst.txt"
-                    try {
-                        timeout(activity: true, time: 120, unit: 'SECONDS') {
-                            input(id: 'kundeInput', message: 'Choose institution', parameters: [
-                                    choice(choices: file,name: 'kunde')
-                            ])
-                        }
-                    } catch (err) {
-                        println("Release aborted")
-                        throw err
-                    }
-                }
-            }
-        }
-*/
-
-/*
-        stage('Pre-build scripts') {
-            steps {
-                echo "Fetching environmentvariables for build from : ${ENV_FOLDER}/env/env.properties"
-                load "${ENV_FOLDER}/env/env.properties"
-                echo "TARGET_FOLDER: ${TARGET_FOLDER}"
-                echo "COMPRESSED_INSTALLER_FILE: ${COMPRESSED_INSTALLER_FILE}"
-                echo "INSTALLER_SCRIPT: ${INSTALLER_SCRIPT}"
-
-                echo "copying local.cfg from environment-project in brage6"
-                sh "cp ${ENV_FOLDER}/env/local_UTVIKLE.cfg dspace/config/local.cfg"
-
-                echo "copying feide.properties from environment-project in brage6"
-                sh "cp ${ENV_FOLDER}/env/feide_UTVIKLE.properties dspace/config/feide.properties"
-
-                echo "copying handleserver.properties from environment-project in brage6"
-                sh "cp ${ENV_FOLDER}/env/handleserver_UTVIKLE.properties dspace/config/handleserver.properties"
-
-                sh "echo jenkins.url = ${JENKINS_URL} >> dspace/config/local.cfg"
-                sh "echo jenkins.tag = ${BUILD_TAG} >> dspace/config/local.cfg"
-                sh "echo git.branch = ${GIT_BRANCH} >> dspace/config/local.cfg"
-                sh "echo git.commit = ${GIT_COMMIT} >> dspace/config/local.cfg"
-            }
-        }
-*/
         stage('Maven Build') {
             steps {
                 echo "Building with maven"
@@ -164,27 +107,6 @@ pipeline {
 			}
 		}
 
-/*
-        stage('Copy files to brage-server, running installer-script') {
-            steps {
-                echo "Destination environment: ${TARGET_HOST}"
-                sh "ssh ${TARGET_HOST} mkdir -p ${TARGET_FOLDER}"
-
-                echo "Compressing dspace/target/dspace-installer to ${COMPRESSED_INSTALLER_FILE}"
-                sh "tar -zcf ${COMPRESSED_INSTALLER_FILE} -C dspace/target dspace-installer"
-
-                echo "Transferring ${COMPRESSED_INSTALLER_FILE} to ${TARGET_HOST}:${TARGET_FOLDER}"
-                sh "scp ${COMPRESSED_INSTALLER_FILE} ${TARGET_HOST}:${TARGET_FOLDER}/"
-
-                echo "Transferring deployscripts to ${TARGET_HOST}:${TARGET_FOLDER}"
-                sh "scp -r deployscripts ${TARGET_HOST}:${TARGET_FOLDER}/"
-                sh "scp -r deployscripts/${INSTALLER_SCRIPT} ${TARGET_HOST}:${TARGET_FOLDER}/"
-
-                echo "Executing ${TARGET_FOLDER}/installer.sh on ${TARGET_HOST}"
-                sh "ssh ${TARGET_HOST} 'source ~/.profile; sh ${TARGET_FOLDER}/${INSTALLER_SCRIPT} unit;'"
-            }
-        }
-*/
         stage('Cleanup') {
             steps {
                 cleanWs()
@@ -192,21 +114,23 @@ pipeline {
         }
     }
 
-//    post {
-//        failure {
-//            script {
-//                def message = "${currentBuild.fullDisplayName} - Failure after ${currentBuild.durationString.replaceFirst(" and counting", "")}"
-//                emailext(
-//                        subject: "FAILURE: ${currentBuild.fullDisplayName}",
-//                        body: "${message}\n Open: ${env.BUILD_URL}",
-//                        to: 'teamrosa@bibsys.no',
-//                        attachlog: true,
-//                        compresslog: true,
-//                        recipientProviders: [[$class: 'CulpritsRecipientProvider']]
-//                )
-//
-//            }
-//        }
-//    }
+/*
+    post {
+        failure {
+            script {
+                def message = "${currentBuild.fullDisplayName} - Failure after ${currentBuild.durationString.replaceFirst(" and counting", "")}"
+                emailext(
+                        subject: "FAILURE: ${currentBuild.fullDisplayName}",
+                        body: "${message}\n Open: ${env.BUILD_URL}",
+                        to: 'teamrosa@bibsys.no',
+                        attachlog: true,
+                        compresslog: true,
+                        recipientProviders: [[$class: 'CulpritsRecipientProvider']]
+                )
+
+            }
+        }
+    }
+*/
 }
 
