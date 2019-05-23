0a1674722f63c3a62d45be4959ccbeae8aeaaac3
pcb <per.christian.bjelke@bibsys.no>
2019-05-23 12:03:22 +0200

    renamed ansible-directory testing vault-connection

--- Jenkinsfile-deploy.groovy
+++ Jenkinsfile-deploy.groovy
@@ -12,42 +12,42 @@ pipeline {
 
     stages {
 
-//		stage('Checkout Brage6 customizations') {
-//            steps {
-//                script {
-//                    brageVars = checkout scm
-//                    dir("${CUSTOMZ}") {
-//                        //configVars = checkout scm
-//                        git url: 'git@git.bibsys.no:team-rosa/brage6-customizations.git'
-//                    }
-//                }
-//            }
-//        }
+		stage('Checkout Brage6 customizations') {
+            steps {
+                script {
+                    brageVars = checkout scm
+                    dir("${CUSTOMZ}") {
+                        //configVars = checkout scm
+                        git url: 'git@git.bibsys.no:team-rosa/brage6-customizations.git'
+                    }
+                }
+            }
+        }
 
-//        stage('Provide input parameters to confirm deployment') {
-//            steps {
-//				script {
-//					def institusjoner = readYaml file: "${CUSTOMZ}/institusjoner.yml"
-//					def kunder = []
-//
-//					institusjoner.each { prop, val ->
-//						kunder << prop
-//					}
-//
-//	                try {
-//	                    timeout(activity: true, time: 120, unit: 'SECONDS') {
-//	                        inputResult = input(id: 'phaseInput', message: 'Velg parametre', parameters: [
-//	                                choice(choices: ["utvikle", "test", "produksjon"], name: 'devstep', description: 'Utviklingsfase:'),
-//									choice(choices: kunder, name: 'kunde', description: "Kunde:")
-//	                        ])
-//	                    }
-//	                } catch (err) {
-//	                    echo "Release aborted"
-//	                    throw err
-//	                }
-//				}
-//            }
-//        }
+        stage('Provide input parameters to confirm deployment') {
+            steps {
+				script {
+					def institusjoner = readYaml file: "${CUSTOMZ}/institusjoner.yml"
+					def kunder = []
+
+					institusjoner.each { prop, val ->
+						kunder << prop
+					}
+
+	                try {
+	                    timeout(activity: true, time: 120, unit: 'SECONDS') {
+	                        inputResult = input(id: 'phaseInput', message: 'Velg parametre', parameters: [
+	                                choice(choices: ["utvikle", "test", "produksjon"], name: 'devstep', description: 'Utviklingsfase:'),
+									choice(choices: kunder, name: 'kunde', description: "Kunde:")
+	                        ])
+	                    }
+	                } catch (err) {
+	                    echo "Release aborted"
+	                    throw err
+	                }
+				}
+            }
+        }
 
 		stage('Bootstrap workspace') {
 			steps {
